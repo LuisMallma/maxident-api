279b8774c5490d2efa2b635db19544ad15b7e695
luis <luis.mch154@gmail.com>
2025-08-21 22:49:47 -0500

    first commit

---
+++ src/main/java/com/maxident/maxident_api/MaxidentApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxident.maxident_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxidentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxidentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxident/maxident_api/MaxidentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxident.maxident_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxidentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
